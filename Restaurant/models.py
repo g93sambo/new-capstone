a27070fc806a538377c908c8cd779d46a386a325
sambo <adetunlesearowolo@gmail.com>
2023-10-22 15:55:21 +0100

    first commit

---
+++ Restaurant/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(max_length=6)
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.Name
+
+class Menu(models.Model):
+    booking_id = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return self.Title
